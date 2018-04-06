5e60d9cc8550eb62ad670f312ce83d08f4ff4b1d
Sean Macfarlane <sean.g.macfarlane@gmail.com>
2018-04-06 03:40:14 -0400

    fixed crash if loop = true and no slides

--- src/siema.js
+++ src/siema.js
@@ -175,7 +175,7 @@ export default class Siema {
     const docFragment = document.createDocumentFragment();
 
     // Loop through the slides, add styling and add them to document fragment
-    if (this.config.loop) {
+    if (this.config.loop && this.innerElements.length > 0) {
       for (let i = this.innerElements.length - this.perPage; i < this.innerElements.length; i++) {
         const element = this.buildSliderFrameItem(this.innerElements[i].cloneNode(true));
         docFragment.appendChild(element);
@@ -185,7 +185,7 @@ export default class Siema {
       const element = this.buildSliderFrameItem(this.innerElements[i]);
       docFragment.appendChild(element);
     }
-    if (this.config.loop) {
+    if (this.config.loop && this.innerElements.length > 0) {
       for (let i = 0; i < this.perPage; i++) {
         const element = this.buildSliderFrameItem(this.innerElements[i].cloneNode(true));
         docFragment.appendChild(element);
